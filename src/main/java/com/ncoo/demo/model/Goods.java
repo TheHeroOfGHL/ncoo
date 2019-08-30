56a02d230f0d682c62d6c5b6803588e5ebc9d2c5
gaohl <17791475280@163.com>
2019-08-30 17:29:31 +0800

    增加实体类

---
+++ src/main/java/com/ncoo/demo/model/Goods.java
@@ -0,0 +1,48 @@
+package com.ncoo.demo.model;
+
+/**
+ * @Description:
+ * @Author: gaohl
+ * @CreateDate: 2019/8/29 18:24
+ */
+public class Goods {
+    private int id;
+
+    private String name;
+
+    private String price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    private String type;
+}
---
+++ src/main/java/com/ncoo/demo/model/User.java
@@ -0,0 +1,70 @@
+package com.ncoo.demo.model;
+
+/**
+ * @Description:
+ * @Author: gaohl
+ * @CreateDate: 2019/8/29 18:19
+ */
+public class User {
+    private int id;
+
+    private String name;
+
+    private String sex;
+
+    private String tel;
+
+    private String password;
+
+    private int type;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
